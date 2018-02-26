1b27ece4cf1e6970fe579554f1f65dcdf409ec86
NataliaKirishun <31582169+NataliaKirishun@users.noreply.github.com>
2018-02-26 19:53:17 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+    equation=equation.split(' ').join('');
+   let arr=equation.split('*x^2'),
+        arr1=arr[1].split('*x'),
+        newArr=[arr[0],...arr1];
+    for (let i=0; i<newArr.length;i++){
+        newArr[i]=Number(newArr[i]);
+    }
+    let D=Math.sqrt(newArr[1]*newArr[1]-4*newArr[0]*newArr[2]);
+     return [Math.round((-newArr[1]+D)/(2*newArr[0])),Math.round((-newArr[1]-D)/(2*newArr[0]))].sort((a,b)=>a-b);
 }
